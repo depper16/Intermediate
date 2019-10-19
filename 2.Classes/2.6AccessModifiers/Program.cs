105485aa8558e3060bf26514fc2140bd93ee0976
depper16 <56670876+depper16@users.noreply.github.com>
2019-10-19 22:04:25 +0700

    2.6 UpdatePerson

--- 2.Classes/2.6AccessModifiers/Program.cs
+++ 2.Classes/2.6AccessModifiers/Program.cs
@@ -8,6 +8,16 @@ namespace AccessModifiers
 {
     public class Person
     {
+        private DateTime _birthdate;
+        public void SetBirthdate(DateTime birthdate)
+        {
+            _birthdate = birthdate;
+        }
+
+        public DateTime GetBirthdate()
+        {
+            return _birthdate;
+        }
 
     }
     class Program
