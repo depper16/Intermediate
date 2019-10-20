4a9a4d312d234d846e8fa58e8b86193f878db51b
depper16 <56670876+depper16@users.noreply.github.com>
2019-10-20 11:56:55 +0700

    3.3 UpdateDbMigrator

--- 3.Association/3.3 Composition/DbMigrator.cs
+++ 3.Association/3.3 Composition/DbMigrator.cs
@@ -1,4 +1,15 @@
 ﻿namespace Composition
 {
     public class DbMigrator
+    {
+        private readonly Logger _logger;
+        public DbMigrator(Logger logger)
+        {
+            _logger = logger;
+        }
+        public void Migrate()
+        {
+            _logger.Log("We are migrating blah blah blah...");
+        }
+    }
 }
