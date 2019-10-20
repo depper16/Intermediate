8b42665b3e381b0911a2ccf58a640df4850a5402
depper16 <56670876+depper16@users.noreply.github.com>
2019-10-20 14:53:38 +0700

    6.2 UpdateMain

--- 6.Interfaces/6.2 Testability/Program.cs
+++ 6.Interfaces/6.2 Testability/Program.cs
@@ -6,10 +6,14 @@ using System.Threading.Tasks;
 
 namespace Testability
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            var orderProcessor = new OrderProcessor(new ShippingCalculator());
+            var order = new Order { DatePlaced = DateTime.Now, TotalPrice = 100f };
+            orderProcessor.Process(order);
         }
     }
 }
