5a73a65235b7deb152e81dfb212a0468f614aabd
depper16 <56670876+depper16@users.noreply.github.com>
2019-10-20 15:46:34 +0700

    6.3 Finsh

--- 6.Interfaces/6.3 Extensibility/Program.cs
+++ 6.Interfaces/6.3 Extensibility/Program.cs
@@ -6,10 +6,13 @@ using System.Threading.Tasks;
 
 namespace Extensiblity
 {
+
     class Program
     {
         static void Main(string[] args)
         {
+            var dbMigrator = new DbMigrator(new FileLogger("C:\\Projects\\log.txt"));
+            dbMigrator.Migrate();
         }
     }
 }
